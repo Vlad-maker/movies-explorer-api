552d13d95c493c7b7b58b68756233c4351e7c145
Vlad-maker <skateislife@mail.ru>
2021-05-08 14:12:36 +0300

    Refactore: app.js routes added

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
+const userRouter = require('./routes/users');
+const moviesRouter = require('./routes/movies');
+const signinUser = require('./routes/signin');
+const signupUser = require('./routes/signup');
 
 const app = express();
+
+app.use(helmet());
 
 const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://localhost:27017/moviesexplorerdb', {
@@ -12,7 +19,16 @@ mongoose.connect('mongodb://localhost:27017/moviesexplorerdb', {
   useFindAndModify: false
 });
 
+app.use(bodyParser.json());
 
+app.use('/', signinUser);
+app.use('/', signupUser);
+app.use('/', authoriz, userRouter);
+app.use('/', authoriz, moviesRouter);
+
+app.use(() => {
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
+});
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`)
