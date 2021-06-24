4677abe884e5dd7f989c2d01afe343a955f65628
Vlad-maker <skateislife@mail.ru>
2021-06-24 17:06:47 +0300

    Refactore: cors options

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { errors } = require('celebrate');
 const helmet = require('helmet');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { BD_DEV_HOST } = require('./utils/config');
-const cors = require('cors')
+const cors = require('cors');
 const userRouter = require('./routes/users');
 const moviesRouter = require('./routes/movies');
 const { authoriz } = require('./middlewares/auth');
@@ -17,24 +17,24 @@ const NotFoundError = require('./errors/NotFoundError');
 
 const app = express();
 
-
 const { PORT = 3000, LINK, NODE_ENV } = process.env;
 
-const corsOptions = {
-  origin: ['*'],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-};
-app.use('*', cors(corsOptions));
+// const corsOptions = {
+//   origin: ['*'],
+//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+//   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+// };
+// app.use('*', cors(corsOptions));
+app.use(cors());
 app.use(helmet());
 
 mongoose.connect(NODE_ENV === 'production' ? LINK : BD_DEV_HOST, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 app.use(bodyParser.json());
@@ -53,5 +53,5 @@ app.use(errors());
 app.use(centralErrors);
 
 app.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`)
-})
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
